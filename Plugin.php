3c2578092d41386d61a2d8d084cb72eb581b8e34
cmfcmf <cmfcmf.flach@gmail.com>
2013-03-03 09:58:04 +0100

    Changed header

--- Plugin.php
+++ Plugin.php
@@ -1,16 +1,9 @@
 <?php
 /**
- * Copyright Zikula Foundation 2012 - Zikula Application Framework
+ * Zikula SystemPlugin introducing TCPDF.
  *
- * This work is contributed to the Zikula Foundation under one or more
- * Contributor Agreements and licensed to You under the following license:
- *
- * @license GNU/LGPLv3 (or at your option, any later version.
- *
- * Please see the NOTICE file distributed with this source code for further
- * information regarding copyright and licensing.
+ * For more information and bug-reporting, visit https://www.github.com/cmfcmf/Tcpdf.
  */
-
 class SystemPlugin_Tcpdf_Plugin extends Zikula_AbstractPlugin implements Zikula_Plugin_AlwaysOnInterface
 {
     /**
